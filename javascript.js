1ab6563d14259915839bbc86c631a0effdb6e05b
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-03-27 22:35:33 +1300

    Comment out Book.prototype.info() which is unneeded

--- javascript.js
+++ javascript.js
@@ -7,12 +7,12 @@ function Book (title, author, pages, read) {
   this.read = read
 }
 
-Book.prototype.info = function () {
-  const readStr = this.read ? 'has read' : 'not read yet'
-  console.log(
-    `${this.title} by ${this.author}, ${this.pages} pages, ${readStr}`
-  )
-}
+// Book.prototype.info = function () {
+//   const readStr = this.read ? 'has read' : 'not read yet'
+//   console.log(
+//     `${this.title} by ${this.author}, ${this.pages} pages, ${readStr}`
+//   )
+// }
 
 function displayBooks () {
   const previousBookCards = document.getElementById('bookCards')
